181fe6d57a2a2d64753c4a54bada1875273836c9
AcusioCEO <AcusioCEO@Gmail.com>
2018-10-03 18:16:10 -0700

    mdl is deprecated, so I am switching to Ant Design & Belle. This is rendering two buttons from AntDesign.

--- mk2/src/App.js
+++ mk2/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+      </div>
+    );
   }
 }
 
