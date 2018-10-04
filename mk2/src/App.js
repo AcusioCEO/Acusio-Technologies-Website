2dc957cfc7b18f3464b67f10fde0b263b4b425d7
AcusioCEO <AcusioCEO@Gmail.com>
2018-10-03 18:58:38 -0700

    Integrated Ant Design... Added the basic layout, now to work on the Navbar...

--- mk2/src/App.js
+++ mk2/src/App.js
@@ -1,15 +1,82 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button } from "antd";
 import "antd/dist/antd.css";
+import { Layout, Card, Row, Col } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Button type="primary">Primary</Button>
-        <Button>Default</Button>
-      </div>
+      <Layout>
+        <Header className="block">
+          Acusio Technologies
+          <span />
+        </Header>
+        <Content>
+          <div style={{ background: "#ECECEC", padding: "30px" }}>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  <img src="https://lh3.googleusercontent.com/-pxjmFmMK8do/AAAAAAAAAAI/AAAAAAAAAAA/AAN31DUFqMLVuezQ0HSkIXh5AaMC-39RXg/mo/photo.jpg?sz=46" />
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+            </Row>
+            <hr />
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+            </Row>
+            <hr />
+
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>
+                  Card content
+                </Card>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+        <Footer>
+          FooterFooterFooterFooterFooterFooterFooterFooterFooterFooter
+          FooterFooterFooterFooterFooterFooterFooterFooterFooterFooterFooter
+          FooterFooterFooterFooterFooterFooterFooterFooterFooterFooterFooter
+        </Footer>
+      </Layout>
     );
   }
 }
