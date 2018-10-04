58f30de78863fabe690ae9cf967f6a421a58e5ff
AcusioCEO <AcusioCEO@Gmail.com>
2018-10-03 17:12:37 -0700

    Added Links for Forum & Project Pages

---
+++ mk2/src/components/Forum.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Forum extends Component {
+  render() {
+    return (
+      <div>
+        <h1> This is your Forum page! </h1>
+      </div>
+    );
+  }
+}
+
+export default Forum;
--- mk2/src/components/Routing.js
+++ mk2/src/components/Routing.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./Home";
+import Forum from "./Forum";
+import Projects from "./Projects";
 import About from "./About";
 import Contact from "./Contact";
 
 const Routing = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route path="/forum" component={Forum} />
+    <Route path="/projects" component={Projects} />
     <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
   </Switch>
